9379eccccf5a085081bfe87981c4d74f5017bd8a
Brian Kardon <brian.kardon@gmail.com>
2022-04-18 14:25:13 -0400

    Removed files duplicated in MATLAB-utils

--- Source/abbreviateText.m
+++
@@ -1,7 +0,0 @@
-function abbreviatedText = abbreviateText(text, maxLen)
-    if numel(text) > maxLen
-        halfWidth = floor((maxLen-3)/2);
-        abbreviatedText = [text(1:halfWidth), '...', text(end-halfWidth:end)];
-    else
-        abbreviatedText = text;
-    end
