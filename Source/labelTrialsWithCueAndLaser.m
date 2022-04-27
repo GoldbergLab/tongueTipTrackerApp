2dc003070b8ac59ba65957564317d02a9d2cab82
Brian Kardon <brian.kardon@gmail.com>
2022-04-27 19:09:49 -0400

    Added handling of unusual abnormal laser signal
    Now ignores any laser signal that occurs before cue time, and any laser pulse that only lasts a single frame.

--- Source/labelTrialsWithCueAndLaser.m
+++ Source/labelTrialsWithCueAndLaser.m
@@ -13,9 +13,9 @@ function labelTrialsWithCueAndLaser(varargin)
 % dryRun (optional, default=1) if true, this command doesn't actually
 %   rename anything, just outputs a record of what it would do. Set to 0 to
 %   actually rename files.
-% queue (optional, dummy queue that displays using disp) a message queue 
-%   for sending stdout messages to parent process, for parallelization 
-%   purposes.
+% queue (optional, default=dummy queue that displays using disp) a message 
+%   queue for sending stdout messages to parent process, for 
+%   parallelization purposes.
 
 p = inputParser;
 addRequired(p, 'xmlDirectory');
@@ -31,7 +31,7 @@ trialExtensions = p.Results.trialExtensions;
 dryRun = p.Results.dryRun;
 queue = p.Results.queue;
 verbose = p.Results.verbose;
-disp(p.Results)
+% disp(p.Results)
 % If no queue given, create a dummy queue
 if isempty(queue)
     queue = parallel.pool.DataQueue();
@@ -96,11 +96,50 @@ for k = 1:length(xmlFilepaths)
     end
 end
 
-function [cueFrame, isLaser] = findTriggerFrameAndLaser(xmlFilepath)
+function [cueFrame, isLaser] = findTriggerFrameAndLaser(xmlFilepath, queue)
+if ~exist('queue', 'var') || isempty(queue)
+    queue = parallel.pool.DataQueue();
+    afterEach(queue, @disp);
+end
+
+% Read XML file as text
 text = fileread(xmlFilepath);
+% Find tag that indicates which frame is cue frame
 tokens = regexp(text, '<FirstImageNo>-([0-9]+)</FirstImageNo>', 'tokens');
-cueFrame = str2num(tokens{1}{1}) + 1;
-if regexp(text, ' E</Time>')
+% Parse cue frame from tag text
+cueFrame = str2double(tokens{1}{1}) + 1;
+% Find list of laser on frame numbers
+laserFrameTokens = regexp(text, '<Time frame=\"(-?[0-9])+\">[0-9\.\ \:]+ E</Time>', 'tokens');
+laserFrames = cellfun(@(f)str2double(f{1}), laserFrameTokens);
+
+% Alert user to potential abnormalities in laser on signal, and attempt to
+% compensate for them.
+[~, name, ext] = fileparts(xmlFilepath);
+if any(laserFrames < 0)
+    send(queue, sprintf('Warning, file %s has laser-on pulses before the cue frame! Ignoring laser signal before cue frame...', [name, ext]));
+    laserFrames(laserFrames < 0) = [];
+end
+if length(laserFrames) == 1
+    send(queue, sprintf('Warning, file %s has only one single frame of post-cue laser-on pulse! This file will be marked as non-laser.', [name, ext]));s
+    laserFrames = [];
+end
+if any(diff(laserFrames) ~= 1)
+    send(queue, sprintf('Warning, file %s has multiple laser-on pulses!', [name, ext]));
+end
+
+% Check for and eliminate single-frame pulses
+laserMask = false(1, max(laserFrames));
+laserMask(laserFrames) = true;
+laserNeighbors = conv(laserMask, [1, 1, 1], 'same');
+laserNeighbors = laserNeighbors(laserMask);
+if any(laserNeighbors < 2)
+    % There are isolated single-frame laser pulses...
+    send(queue, sprintf('Warning, file %s has single-frame laser-on pulses! Eliminating them...', [name, ext]));
+    laserFrames(laserNeighbors < 2) = [];
+end
+
+% Determine if 
+if length(laserFrames) > 1
     isLaser = true;
 else
     isLaser = false;
