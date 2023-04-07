35dc9be4a4160aac427a02d62f4cb4283591f36b
Goldberlab <Goldberglab@as-nbb-gold-25.ad.cornell.edu>
2023-04-07 14:19:35 -0400

    Initialize rw_licks and prev_licks fields properly
    Without erasing content

--- Source/make_rw_struct_2D.m
+++ Source/make_rw_struct_2D.m
@@ -2,6 +2,12 @@ function [rw_trial_struct] = make_rw_struct_2D(nl_struct)
 rw_struct_number = 1;
 trial_num = 1;
 rw_trial_struct = struct;
+
+% Initialize rw_licks and prev_licks fields, in case no data gets assigned
+% to them later
+rw_trial_struct.rw_licks = [];
+rw_trial_struct.prev_licks = [];
+
 first_time = 1;
 
 for i = 1:numel(nl_struct)
@@ -34,8 +40,6 @@ for i = 1:numel(nl_struct)
             rw_trial_struct(trial_num).analog_lick = analog_lick(rw_cue(j,1):rw_cue(j,2));
             rw_trial_struct(trial_num).actuator1_ML_command = actuator1_ML_command(rw_cue(j,1):rw_cue(j,2));
             rw_trial_struct(trial_num).actuator2_AP_command = actuator2_AP_command(rw_cue(j,1):rw_cue(j,2));
-%             rw_trial_struct(trial_num).rw_licks = [];
-%             rw_trial_struct(trial_num).prev_licks = [];
             
             if(isempty(dispense)==0)
                 
@@ -44,53 +48,53 @@ for i = 1:numel(nl_struct)
                 rw_trial_struct(trial_num).dispense =  dispense_in_cue-rw_cue(j,1);
                 
                 rw_trial_struct(trial_num).laser_onset_rel_disp = laser_onset(j)-dispense_in_cue;
-                
-                if numel(licks)
-                    licks_in_cue_orig = licks(:,1);
-                    prev_lick_orig = prev_licks;
-                else
-                    licks_in_cue_orig = [];
-                    prev_lick_orig = [];
-                end
-                
-                licks_in_cue = licks_in_cue_orig((licks_in_cue_orig>rw_cue(j,1))&(licks_in_cue_orig<(rw_cue(j,1)+1300)));
-                licks_in_cue = licks_in_cue - rw_cue(j,1);
-                lick_ili = diff(licks_in_cue);
-                
-                prev_lick_orig  = prev_lick_orig((licks_in_cue_orig>rw_cue(j,1))&(licks_in_cue_orig<(rw_cue(j,1)+1300)));
-                
-                try
-                    retrieval_licks = [licks_in_cue(1)];
-                    prev_licks_vect = [prev_lick_orig(1)];
-                    k=1;
-                    retrieval_ilis=[];
-                   
-                    while 1
-                        retrieval_licks = [retrieval_licks licks_in_cue(k+1)];                        
-                        retrieval_ilis = [retrieval_ilis lick_ili(k)];
-                        prev_licks_vect = [prev_licks_vect prev_lick_orig(k+1)];
-                        k=k+1;
-                        if k == length(lick_ili) + 1
-                            break
-                        end
-                    end
-                    
-                    rw_trial_struct(trial_num).rw_licks = retrieval_licks;
-                    rw_trial_struct(trial_num).rw_ili = retrieval_ilis;
-                    rw_trial_struct(trial_num).prev_licks = prev_licks_vect;
-                catch
-                   if numel(licks_in_cue)
-                    rw_trial_struct(trial_num).rw_licks = retrieval_licks;
-                    rw_trial_struct(trial_num).prev_licks = prev_licks_vect;%doesnt record if only one lick(dispense)
-                   else
-                    rw_trial_struct(trial_num).rw_licks = [];
-                    rw_trial_struct(trial_num).prev_licks = [];
-                   end    
-                    rw_trial_struct(trial_num).rw_ili = [];
-                end
             else
                 rw_trial_struct(trial_num).dispense = [];
                 rw_trial_struct(trial_num).laser_onset_rel_disp =[];
+            end
+                
+            if numel(licks)
+                licks_in_cue_orig = licks(:,1);
+                prev_lick_orig = prev_licks;
+            else
+                licks_in_cue_orig = [];
+                prev_lick_orig = [];
+            end
+
+            licks_in_cue = licks_in_cue_orig((licks_in_cue_orig>rw_cue(j,1))&(licks_in_cue_orig<(rw_cue(j,1)+1300)));
+            licks_in_cue = licks_in_cue - rw_cue(j,1);
+            lick_ili = diff(licks_in_cue);
+
+            prev_lick_orig  = prev_lick_orig((licks_in_cue_orig>rw_cue(j,1))&(licks_in_cue_orig<(rw_cue(j,1)+1300)));
+
+            try
+                retrieval_licks = [licks_in_cue(1)];
+                prev_licks_vect = [prev_lick_orig(1)];
+                k=1;
+                retrieval_ilis=[];
+
+                while 1
+                    retrieval_licks = [retrieval_licks licks_in_cue(k+1)];                        
+                    retrieval_ilis = [retrieval_ilis lick_ili(k)];
+                    prev_licks_vect = [prev_licks_vect prev_lick_orig(k+1)];
+                    k=k+1;
+                    if k == length(lick_ili) + 1
+                        break
+                    end
+                end
+
+                rw_trial_struct(trial_num).rw_licks = retrieval_licks;
+                rw_trial_struct(trial_num).rw_ili = retrieval_ilis;
+                rw_trial_struct(trial_num).prev_licks = prev_licks_vect;
+            catch
+               if numel(licks_in_cue)
+                rw_trial_struct(trial_num).rw_licks = retrieval_licks;
+                rw_trial_struct(trial_num).prev_licks = prev_licks_vect;%doesnt record if only one lick(dispense)
+               else
+                rw_trial_struct(trial_num).rw_licks = [];
+                rw_trial_struct(trial_num).prev_licks = [];
+               end    
+                rw_trial_struct(trial_num).rw_ili = [];
             end
             
             rw_trial_struct(trial_num).real_time = datenum(ref_time) + (rw_trial_struct(trial_num).frame_num-ref_frame_num)/(24*60*60) + (rw_trial_struct(trial_num).rw_cue(1))/(24*3600*1000);
