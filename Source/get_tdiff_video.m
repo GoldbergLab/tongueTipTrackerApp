89f895bbe2481541f45fa6ba53588a20f457a04e
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-08-21 12:43:21 -0400

    Integrate proper filename timestamp parsing into get_tdiff_video
    Also:
    Make parsePCCFilenameTimestamp handle single char array timestamps as well as cell arrays of them
    Add explicit call to timestamp parser function in tongueTipTrackerApp

--- Source/get_tdiff_video.m
+++ Source/get_tdiff_video.m
@@ -1,46 +1,38 @@
-function [tdiff_sp, tdiff_vid, result] = get_tdiff_video(sessionVideoRoots, FPGADataRoots)
-result = true;
-for sessionNum=1:numel(sessionVideoRoots)
-    videoList = dir([sessionVideoRoots{sessionNum},'\*.avi']);
-    try
-    for videoNum=1:numel(videoList)
-        [~, videoName, ~] = fileparts(videoList(videoNum).name);
-        videoNameParts = strsplit(videoName);
-        dateParts = videoNameParts(3:7);
-        days = str2num(dateParts{1});
-        hours = str2num(dateParts{3});
-        minutes = str2num(dateParts{4});
-        seconds = str2num(dateParts{5});
-        trial_time(videoNum) = days + hours/24 + minutes/(24*60)+seconds/(24*60*60);
-    end
-    catch e
-        disp(e)
-        result='Failed to parse the video timestamp. Make sure videos are saved with the correct video timestamp format.';
-        tdiff_sp = [];
-        tdiff_vid = [];
-        return
-    end
-    tdiff_vid{sessionNum} = diff(trial_time);
-    clear trial_time
+function [tdiff_fpga, tdiff_vid, result] = get_tdiff_video(sessionVideoRoots, sessionFPGARoots, timestampParser)
+% Get intertrial intervals for each session based on both video and fpga
+% data streams
+%
+%   timestampParser = an optional timestamp parser function (see
+%       sortFilesByTimestamp). If omitted, defaults to the
+%       parsePCCFilenameTimestamp function
+
+% Check if we need default timestamp parser
+if ~exist('timestampParser', 'var') || isempty(timestampParser)
+    timestampParser = @parsePCCFilenameTimestamp;
 end
 
-for sessionNum=1:numel(FPGADataRoots)
+% Loop over video session directories
+result = true;
+
+% Initialize tdiff arrays
+tdiff_vid = cell(size(sessionVideoRoots));
+tdiff_fpga = cell(size(sessionFPGARoots));
+
+% Get video trial time differences
+for sessionNum = 1:numel(sessionVideoRoots)
+    % Parse video filename timestamps
+    [~, videoTimestamps] = findSessionVideos(sessionVideoRoots{sessionNum}, 'avi', timestampParser);
+    tdiff_vid{sessionNum} = days(diff(videoTimestamps));
+end
+
+% Get fpga trial time differences
+for sessionNum=1:numel(sessionFPGARoots)
+    % Load lick_struct
     try
-        load(fullfile(FPGADataRoots{sessionNum},'lick_struct.mat'), 'lick_struct');
+        load(fullfile(sessionFPGARoots{sessionNum},'lick_struct.mat'), 'lick_struct');
     catch e
-        result = ['Failed to load lick_struct.mat at ', fullfile(FPGADataRoots{sessionNum},'lick_struct.mat')];
+        result = ['Failed to load lick_struct.mat at ', fullfile(sessionFPGARoots{sessionNum},'lick_struct.mat')];
     end
         
-    tdiff_sp{sessionNum} = diff([lick_struct.real_time]);
+    tdiff_fpga{sessionNum} = diff([lick_struct.real_time]);
 end
-
-% %% remove everything below
-% dirlist = rdir('Top*');
-% for i = 1:numel(dirlist) 
-%     name = dirlist(i).name;
-%     split_name1 = strsplit(name,'p');
-%     split_name2 = strsplit(split_name1{2},'.');
-%     split_name1{2} = strcat(sprintf('%03d',str2num(split_name2{1})-28),'.mat');
-%     
-%     movefile(name,strcat(split_name1{1},'p',split_name1{2}));
-% end
