5e47383bc0667afc849055f17bd6ba3e8255d5f6
Brian Kardon <brian.kardon@gmail.com>
2023-10-13 19:03:44 -0400

    Fixed mapVideoIndexToFPGATrialIndex
    Function was not handling timestamps that went from previous day through midnight of next day.  Fixed.

--- Source/mapVideoIndexToFPGATrialIndex.m
+++ Source/mapVideoIndexToFPGATrialIndex.m
@@ -1,10 +1,10 @@
-function vid_index = mapVideoIndexToFPGATrialIndex(vid_real_time, lick_struct, time_aligned_trial)
+function vid_index = mapVideoIndexToFPGATrialIndex(cue_time_video, lick_struct, time_aligned_trial)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % mapVideoIndexToFPGATrialIndex: Get mapping between video and FPGA trials
 % usage:  vid_index = mapVideoIndexToFPGATrialIndex(vid_real_time, time_aligned_trial)
 %
 % where,
-%    vid_real_time is a list of video timestamps in units of days
+%    cue_time_video is a list of video timestamps in units of days
 %    time_aligned_trials is a 1x2 array, where element 1 is the first video
 %       trial number that corresponds to a FPGA trial, and element 2 is the
 %       FPGA trial that corresponds to that video trial.
@@ -25,20 +25,19 @@ function vid_index = mapVideoIndexToFPGATrialIndex(vid_real_time, lick_struct, t
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Align Time stamps
-spout_time = mod(lick_struct(time_aligned_trial(2)).real_time,1);
-vid_time = vid_real_time(time_aligned_trial(1));
 
-tdiff = (spout_time - vid_time);
+% Collect cue times in lick_stuct time base
+cue_time_lick_struct = [lick_struct.real_time];
+% Make lick-struct cue times relative to start of session
+cue_time_lick_struct = cue_time_lick_struct - cue_time_lick_struct(time_aligned_trial(2));
 
-vid_real_time = vid_real_time + tdiff;
-
-spout_time_vect = [lick_struct.real_time];
-spout_time_vect = mod(spout_time_vect,1);
+% Make video cue times relative to start of session
+cue_time_video = cue_time_video - cue_time_video(time_aligned_trial(1));
 
 %% find corresponding spout trials
 vid_index = [];
-for i=1:numel(vid_real_time)       
-    [min_val(i),vid_index(i)] = min(abs(spout_time_vect-vid_real_time(i)));        
+for i=1:numel(cue_time_video)       
+    [min_val(i),vid_index(i)] = min(abs(cue_time_lick_struct-cue_time_video(i)));        
     if min_val(i)>2*10^(-5)
         vid_index(i) = nan;
     end
